c5c5eb53f8c30b61af8a7b0b2e20b441282f5596
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-09-02 14:21:00 +0900

    deploy

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,7 +12,7 @@
         .card-body
           section
             p
-              | I am a * engineer making web apps, modules, Windows apps and .NET libraries.
+              | I am a ████ making web apps, modules, Windows apps and .NET libraries.
               br
               | If you are interested in me, please see also 
               router-link(to="projects") projects
