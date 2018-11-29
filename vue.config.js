7b72df882bb55541a6a90baa5fb2bda517f15abb
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-11-29 21:12:30 +0900

    feat: optimize css

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,14 @@
+const glob = require("glob");
+const path = require("path");
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+
 module.exports = {
   chainWebpack: (config) => {
     config.module
       .rule('pug')
       .use('pug-plain-loader')
+    config.plugin("purgecss").use(PurgecssPlugin, [ {
+      paths: glob.sync(`${path.join(__dirname, "src")}/**/*.*`)
+    } ])
   }
 }
