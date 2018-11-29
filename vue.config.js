7a0e96ee30dd0d615958adb93d9e480a63c6d4e6
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-11-29 21:14:53 +0900

    Revert "feat: optimize css"
    This reverts commit 7b72df882bb55541a6a90baa5fb2bda517f15abb.

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,7 @@
-const glob = require("glob");
-const path = require("path");
-const PurgecssPlugin = require('purgecss-webpack-plugin')
-
 module.exports = {
   chainWebpack: (config) => {
     config.module
       .rule('pug')
       .use('pug-plain-loader')
-    config.plugin("purgecss").use(PurgecssPlugin, [ {
-      paths: glob.sync(`${path.join(__dirname, "src")}/**/*.*`)
-    } ])
   }
 }
