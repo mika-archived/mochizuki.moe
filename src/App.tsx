59962b3e712cf158a0a69adca8c128b32a815150
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-12-08 08:54:09 +0900

    chore: start developing v4

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import RootPage from "./components/pages/RootPage";
-
-const App: React.FC = () => {
-  return <RootPage />;
-};
-
-export default App;
--- src/components/atoms/Heading/index.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Heading = styled.h1`
-  font-family: Consolas, "Courier New", monospace;
-  font-size: 36px;
-  font-weight: normal;
-`;
-
-export default Heading;
