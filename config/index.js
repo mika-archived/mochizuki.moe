a85158094c25095d013fedcd6c75be134d1c46ee
Mikazuki <mikazuki_fuyuno@outlook.com>
2021-03-18 21:09:49 +0900

    fix: use dist as output directory

--- config/index.js
+++ config/index.js
@@ -54,7 +54,7 @@ module.exports = {
     index: path.resolve(__dirname, '../index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
