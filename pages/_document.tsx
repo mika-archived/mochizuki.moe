4c9f8c26f4ad57d874af0bf2b858d40a68f0e540
Mikazuki <mikazuki_fuyuno@outlook.com>
2021-04-09 13:26:57 +0900

    feat: add lang to `html`

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class CustomDocument extends Document {
+  render() {
+    return (
+      <Html lang="ja">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
